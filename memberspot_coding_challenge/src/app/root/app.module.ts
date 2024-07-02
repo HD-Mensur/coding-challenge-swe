ebc8929623f82141f8c2dc5f9f5f0a1ba40f39ce
HD_Mensur <mensur.grisevic@gmail.com>
2024-07-02 03:56:18 +0200

    cleanup of redundant files and imports

--- memberspot_coding_challenge/src/app/root/app.module.ts
+++ memberspot_coding_challenge/src/app/root/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PeopleListDisplayModule } from '../people-list-display/people-list-display.module';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
 import { RequestCachingInterceptor } from '../interceptors/request-caching.interceptor';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PeopleListDisplayModule } from '../people-list-display/people-list-display.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,6 +16,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     PeopleListDisplayModule,
     BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [
     {
